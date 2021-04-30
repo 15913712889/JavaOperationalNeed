024b817e910e033a73596971935eabb70577f20b
蔡序强 <1781088743@qq.com>
2021-04-30 10:51:51 +0800

    mybatis plus生成器

--- src/main/java/cn/caixiaobai/study/mybatisplus_mbg/CodeGenerator.java
+++ src/main/java/cn/caixiaobai/study/mybatisplus_mbg/CodeGenerator.java
@@ -18,8 +18,6 @@ public class CodeGenerator {
         AutoGenerator generator = new AutoGenerator();
         // set freemarker engine
         generator.setTemplateEngine(new FreemarkerTemplateEngine());
-
-
         //全局配置 GlobalConfig
         GlobalConfig globalConfig = new GlobalConfig();
         //设置生成的目录
@@ -38,23 +36,17 @@ public class CodeGenerator {
         globalConfig.setDateType(DateType.ONLY_DATE);
         //设置swagger
         // globalConfig.setSwagger2(true);
-
         //将配置好的全局配置，配置进代码生成器里
         generator.setGlobalConfig(globalConfig);
-
-
         DataSourceConfig dsc = new DataSourceConfig();
         //设置数据库配置
         dsc.setDbType(DbType.MYSQL);
         //mysql8.0之后要用下面这个驱动
-        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
-        dsc.setUrl("jdbc:mysql://localhost:3306/study?useUnicode=true&allowMultiQueries=true&characterEncoding=UTF-8&useSSL=false");
-        dsc.setUsername("root");
-        dsc.setPassword("root");
-
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUrl("jdbc:mysql:///InMgn?useUnicode=true&allowMultiQueries=true&characterEncoding=UTF-8&useSSL=false");
+        dsc.setUsername("");
+        dsc.setPassword("");
         generator.setDataSource(dsc);
-
-
         //设置包的配置
         PackageConfig pc = new PackageConfig();
         //设置模块名
@@ -62,54 +54,37 @@ public class CodeGenerator {
         //设置模块的父包
         pc.setParent("cn.caixiaobai");
         //设置实体类的的包名
-        pc.setEntity("pojo");
+        //pc.setEntity("pojo");
         //pc.setController("controller");
        // pc.setService("service");
-        pc.setMapper("dao");
-
+        //pc.setMapper("dao");
         //设置全部文件生成在一个文件夹中，方便删除
         pc.setController("other");
-        //pc.setEntity("other");
-        //pc.setMapper("mapper");
+        pc.setEntity("other");
+        pc.setMapper("other");
         pc.setService("other");
         pc.setServiceImpl("other");
         pc.setXml("other");
-
         generator.setPackageInfo(pc);
-
-
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
-
         //设置生成哪些表,如果不设置的话，默认生成当前数据源中的所有表
-        strategy.setInclude("user");
-
+        strategy.setInclude("ea_xiaonei_room");
         //设置包的命名规则
         strategy.setNaming(NamingStrategy.underline_to_camel);
         //设置字段的命名规则
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-
         //设置lombok
         //strategy.setEntityLombokModel(true);
-
         //逻辑删除
         //自动填充配置
-
-
-
-
         //设置乐观锁
         //strategy.setVersionFieldName("version");
         //开启restful的驼峰命名格式
         strategy.setRestControllerStyle(true);
         //设置请求中使用下划线命名而不是驼峰命名
         //strategy.setControllerMappingHyphenStyle(true);
-
-
         generator.setStrategy(strategy);
-
-
-
         //执行方法
         generator.execute();
     }
