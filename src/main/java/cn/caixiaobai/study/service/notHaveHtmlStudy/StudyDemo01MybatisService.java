f931c4ed28e78a2e0177d938d19f3d62205f8925
蔡序强 <1781088743@qq.com>
2021-05-25 11:16:52 +0800

    study

---
+++ src/main/java/cn/caixiaobai/study/service/notHaveHtmlStudy/StudyDemo01MybatisService.java
@@ -0,0 +1,19 @@
+package cn.caixiaobai.study.service.notHaveHtmlStudy;
+
+import cn.caixiaobai.study.result.ResultVo;
+
+/**
+ * created with Intellij IDEA
+ * user: Administrator
+ * date: 2021/05/24
+ * time: 16:15
+ * describe(描述):
+ * to change this template use file | settings| Editor | file and code Templates | includes | file header .
+ */
+public interface StudyDemo01MybatisService {
+    /**
+     * 测试批量插入
+     * @return
+     */
+    ResultVo demo01();
+}
